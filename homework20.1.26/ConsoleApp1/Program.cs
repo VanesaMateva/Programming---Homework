50d7fb3cd4621915350533d1332f0ed7c53be4ac
VanesaMateva <vm42124027@edu1.pgzaimov.com>
2026-01-20 19:04:07 +0200

    Add files via upload

---
+++ homework20.1.26/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = 5740;
+            int years = days / 365;
+            int months = (days % 365) / 30;
+            int day = (days % 365) % 30;
+            int birthyear = 2025 - years;
+            int birthmonth = 13 - months;
+            int birthday = 40 - day;
+            Console.WriteLine($"{birthday}.{birthmonth}.{birthyear}");
+        }//ne znam dali e taka ama poluchih data
+    }
+}
