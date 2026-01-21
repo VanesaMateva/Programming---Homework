f764dd784ae7e510f1b475453260767e5a8c280f
VanesaMateva <vm42124027@edu1.pgzaimov.com>
2026-01-21 21:30:25 +0200

    Add files via upload

---
+++ homework21.1.26/zad5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            string fname = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lname = Console.ReadLine();
+            Console.Write("Enter age: ");
+            sbyte age = sbyte.Parse(Console.ReadLine());
+            Console.Write("Enter your gender(m/f): ");
+            char gender = char.Parse(Console.ReadLine());
+            Console.Write("Enter your PID: ");
+            long pid = long.Parse(Console.ReadLine());
+            Console.Write("Enter your UEN: ");
+            long uen = long.Parse(Console.ReadLine());
+            Console.WriteLine($"First name: {fname}\nLast name: {lname}\nAge: {age}\nGender: {gender}\nPersonal ID: {pid}\nUnique Employee number: {uen}");
+        }
+    }
+}
