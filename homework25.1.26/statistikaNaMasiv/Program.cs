f608742e1c0e76403f19ea580255bac17b8286cc
VanesaMateva <vm42124027@edu1.pgzaimov.com>
2026-01-26 23:29:22 +0200

    Add files via upload

---
+++ homework25.1.26/statistikaNaMasiv/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statistikaNaMasiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 6 numbers: ");
+            int[] numbers = new int[6];
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                numbers[i] = a;
+                if (a < min)
+                    min = a;
+                if (a > max)
+                    max = a;
+                sum += a;
+            }
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {sum / 6}");
+        }
+    }
+}
