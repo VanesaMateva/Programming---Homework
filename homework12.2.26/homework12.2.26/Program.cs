44d04dc185a619e833914254266ab044cd3582e3
VanesaMateva <vm42124027@edu1.pgzaimov.com>
2026-02-12 21:37:03 +0200

    Add files via upload

---
+++ homework12.2.26/homework12.2.26/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework12._2._26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter nums separated with a space:");
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> otriNums = new List<int>();
+            List<int> chetNums = new List<int>();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] % 2 == 0)
+                    chetNums.Add(nums[i]);
+                else 
+                    otriNums.Add(nums[i]);
+            }
+            Console.WriteLine($"All even nums: {string.Join(", ", chetNums)}");
+            Console.WriteLine($"All odd nums: {string.Join(", ", otriNums)}");
+        }
+    }
+}
---
+++ homework12.2.26/homework12.2.26/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("homework12.2.26")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("homework12.2.26")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("68325c78-e2dc-4089-ba5b-dbf60004466c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
